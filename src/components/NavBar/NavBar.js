978e84796c664a1af6283f866f0a1a0a0e2244a9
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-11-06 03:55:04 -0600

    styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import ProfilePic from '../ProfilePic/ProfilePic';
+import profile from '../../assets/profilepic200x200.png';
 
 import classes from './NavBar.module.css';
 
 const navBar = () => {
     return (
-        <header className={classes.NavBar}>
-            <div className={classes.ProfilePic}>
-                <ProfilePic />
+        <header className={classes.Header}>
+            <div className={classes.NavBar}>
+                <img className={classes.ProfilePic} src={profile} alt="Christopher Allen's profile" />
+                <NavLink to="/" className={classes.PName}>Christopher Allen</NavLink>
+                <nav className={classes.Navigation}>
+                    <ul>
+                        <li>
+                            <p><NavLink to="/projects/">projects</NavLink></p>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <h1><NavLink to="/">Christopher Allen</NavLink></h1>
-                    </li>
-                    <li>
-                        <p><NavLink to="/projects/">projects</NavLink></p>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 };
